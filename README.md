fe44bcc5ea6d1ed612a5c0ee71bc6ddbf93432db
闫昊 <yanhao@dces.cn>
2023-07-27 14:26:02 +0800

    first comit

---
+++ README.md
@@ -0,0 +1,1 @@
+# loaf-on-a-job-Mail
---
+++ src/main/java/com/example/loafonajobmail/LoafOnAJobMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.loafonajobmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoafOnAJobMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoafOnAJobMailApplication.class, args);
+    }
+
+}
